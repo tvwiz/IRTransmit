a6f380d04a0014641950673896fa70606beb7b41
bhargavakumark <bhargavakumark@gmail.com>
2016-02-25 16:22:00 +0530

    removing com folder

--- www/irtransmit.js
+++ www/irtransmit.js
@@ -1,29 +1,26 @@
-cordova.define("com.bhargavakumark.irtransmit.IRTransmit", function(require, exports, module) {
-    var irtransmit = {
-	transmit: function(frequency, signal,successCallback, errorCallback){
-	    cordova.exec(successCallback,
-		errorCallback,
-		"IRTransmit",
-		"transmit",
-		[{
-		    "frequency": frequency,
-		    "patten": pattern
-		}]
-	    );
-	},
+var irtransmit = {
+    transmit: function(frequency, signal,successCallback, errorCallback){
+	cordova.exec(successCallback,
+	    errorCallback,
+	    "IRTransmit",
+	    "transmit",
+	    [{
+		"frequency": frequency,
+		"patten": pattern
+	    }]
+	);
+    },
 
-	hasIrEmitter: function(successCallback, errorCallback) {
-	    console.log("hasIrEmitter: inside");
-	    cordova.exec(successCallback,
-		errorCallback,
-		"IRTransmit",
-		"hasIrEmitter",
-		[{
-		}]
-	    );
-	}
+    hasIrEmitter: function(successCallback, errorCallback) {
+	console.log("hasIrEmitter: inside");
+	cordova.exec(successCallback,
+	    errorCallback,
+	    "IRTransmit",
+	    "hasIrEmitter",
+	    [{
+	    }]
+	);
     }
+}
 
-    module.exports = irtransmit;
-
-});
+module.exports = irtransmit;
